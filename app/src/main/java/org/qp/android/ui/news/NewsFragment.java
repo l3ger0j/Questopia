a5dd2c19655b75d9e29998c33e423303126d1af9
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2023-07-27 18:35:25 +0300

    Working on a fragment_news

--- app/src/main/java/org/qp/android/ui/news/NewsFragment.java
+++ app/src/main/java/org/qp/android/ui/news/NewsFragment.java
@@ -1,5 +1,7 @@
 package org.qp.android.ui.news;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ import org.qp.android.R;
 import org.qp.android.databinding.FragmentNewsBinding;
 
 public class NewsFragment extends Fragment {
+
+    private FragmentNewsBinding binding;
 
     @Nullable
     @Override
@@ -32,6 +36,20 @@ public class NewsFragment extends Fragment {
         requireActivity().getOnBackPressedDispatcher()
                 .addCallback(getViewLifecycleOwner(), callback);
 
-        return FragmentNewsBinding.inflate(getLayoutInflater()).getRoot();
+        binding = FragmentNewsBinding.inflate(getLayoutInflater());
+        binding.bannerImage1.setOnClickListener(list -> {
+            var intentImg0 = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://t.me/joinchat/AAAAAFgqAMXq0SA34umFbQ"));
+            intentImg0.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intentImg0);
+        });
+        binding.bannerImage2.setOnClickListener(l -> {
+            var intentImg1 = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://schoollife.fludilka.su/viewtopic.php"));
+            intentImg1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intentImg1);
+        });
+
+        return binding.getRoot();
     }
 }
