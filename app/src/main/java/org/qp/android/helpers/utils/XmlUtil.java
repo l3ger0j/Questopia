6cd732c072c356ceac535ebd48481a25a7469c30
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2023-12-29 22:59:45 +0300

    Exception -> IOException

--- app/src/main/java/org/qp/android/helpers/utils/XmlUtil.java
+++ app/src/main/java/org/qp/android/helpers/utils/XmlUtil.java
@@ -3,17 +3,18 @@ package org.qp.android.helpers.utils;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 public final class XmlUtil {
 
-    public static void objectToXml(OutputStream out , Object o) throws Exception {
+    public static void objectToXml(OutputStream out , Object o) throws IOException {
         new XmlMapper()
                 .enable(SerializationFeature.INDENT_OUTPUT)
                 .writeValue(out , o);
     }
 
-    public static <T> T xmlToObject(String xml, Class<T> clazz) throws Exception {
+    public static <T> T xmlToObject(String xml, Class<T> clazz) throws IOException {
         return new XmlMapper().readValue(xml , clazz);
     }
 }
