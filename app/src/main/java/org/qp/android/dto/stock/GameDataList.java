188e4ec984fd17d80470b1c3363136bfb4a3bd22
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2023-12-29 22:58:43 +0300

    Renamed GameList to GameDataList

--- app/src/main/java/org/qp/android/dto/stock/GameDataList.java
+++ app/src/main/java/org/qp/android/dto/stock/GameDataList.java
@@ -1,5 +1,6 @@
 package org.qp.android.dto.stock;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import java.util.ArrayList;
@@ -7,15 +8,16 @@ import java.util.List;
 import java.util.Objects;
 
 @JsonRootName(value = "game_list")
-public class GameList {
+public class GameDataList {
 
+    @JsonAlias({"game"})
     public List<GameData> gameDataList = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        var that = (GameList) o;
+        var that = (GameDataList) o;
         return Objects.equals(gameDataList , that.gameDataList);
     }
 
