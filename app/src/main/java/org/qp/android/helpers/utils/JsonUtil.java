4bacfaf7016b37a795820110be44149aa977e707
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2024-01-15 20:48:28 +0300

    Fixed No content to map due to end-of-input

--- app/src/main/java/org/qp/android/helpers/utils/JsonUtil.java
+++ app/src/main/java/org/qp/android/helpers/utils/JsonUtil.java
@@ -1,5 +1,6 @@
 package org.qp.android.helpers.utils;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,24 +13,32 @@ public class JsonUtil {
 
     public static void objectToJson(OutputStream out , Object o) throws IOException {
         new ObjectMapper()
+                .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .enable(SerializationFeature.INDENT_OUTPUT)
                 .writeValue(out , o);
     }
 
     public static void objectToJson(File file , Object o) throws IOException {
         new ObjectMapper()
+                .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .writeValue(file , o);
     }
 
     public static <T> T jsonToObject(String json, Class<T> clazz) throws IOException {
-        return new ObjectMapper().readValue(json , clazz);
+        return new ObjectMapper()
+                .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
+                .readValue(json , clazz);
     }
 
     public static <T> T jsonToObject(File file, Class<T> clazz) throws IOException {
-        return new ObjectMapper().readValue(file , clazz);
+        return new ObjectMapper()
+                .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
+                .readValue(file , clazz);
     }
 
     public static <T> T jsonToObject(File file, TypeReference<T> ref) throws IOException {
-        return new ObjectMapper().readValue(file , ref);
+        return new ObjectMapper()
+                .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
+                .readValue(file , ref);
     }
 }
