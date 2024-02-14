9f6bd00f6b6a43693343eed893169eb644e0dbc9
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2024-02-14 21:11:04 +0300

    Refactored settings [0/?]

--- app/src/main/java/org/qp/android/ui/settings/SettingImageFragment.java
+++ app/src/main/java/org/qp/android/ui/settings/SettingImageFragment.java
@@ -3,7 +3,6 @@ package org.qp.android.ui.settings;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.PreferenceFragmentCompat;
 
 import org.qp.android.R;
@@ -15,17 +14,5 @@ public class SettingImageFragment extends PreferenceFragmentCompat {
                                     @Nullable String rootKey) {
         requireActivity().setTitle(R.string.imageCatTitle);
         addPreferencesFromResource(R.xml.setting_image);
-
-        var viewModel =
-                new ViewModelProvider(requireActivity()).get(SettingsViewModel.class);
-
-        var customWidthImagePref = findPreference("customWidth");
-        if (customWidthImagePref != null) {
-            customWidthImagePref.setEnabled(!viewModel.getSettingsController().isUseAutoWidth);
-        }
-        var customHeightImagePref = findPreference("customHeight");
-        if (customHeightImagePref != null) {
-            customHeightImagePref.setEnabled(!viewModel.getSettingsController().isUseAutoHeight);
-        }
     }
 }
