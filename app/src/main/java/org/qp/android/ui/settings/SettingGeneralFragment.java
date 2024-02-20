a7c4b2cff20cf359455ae24bc0ac8d25f8a907df
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2024-02-20 17:48:54 +0300

    Added a visual indicator of the visibility of the separator

--- app/src/main/java/org/qp/android/ui/settings/SettingGeneralFragment.java
+++ app/src/main/java/org/qp/android/ui/settings/SettingGeneralFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import org.qp.android.R;
 import org.qp.android.helpers.utils.ViewUtil;
@@ -19,6 +20,19 @@ public class SettingGeneralFragment extends PreferenceFragmentCompat
                                     @Nullable String rootKey) {
         requireActivity().setTitle(R.string.generalCatTitle);
         addPreferencesFromResource(R.xml.setting_general);
+
+        var prefVizSep = findPreference("separator");
+        if (prefVizSep != null) {
+            var switchPrefVizSep = (SwitchPreferenceCompat) prefVizSep;
+            switchPrefVizSep.setOnPreferenceClickListener(preference -> {
+                if (switchPrefVizSep.isChecked()) {
+                    preference.setIcon(R.drawable.baseline_visibility_off_24);
+                } else {
+                    preference.setIcon(R.drawable.baseline_visibility_24);
+                }
+                return true;
+            });
+        }
     }
 
     @Override
