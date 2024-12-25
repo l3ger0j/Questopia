b1d80b7353fe49845d59ea41831ed1e185eff096
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2024-12-26 00:48:17 +0300

    Refactored `Bind`

--- app/src/main/java/org/qp/android/helpers/Bind.java
+++ app/src/main/java/org/qp/android/helpers/Bind.java
@@ -1,5 +1,8 @@
 package org.qp.android.helpers;
 
+import static org.qp.android.helpers.utils.StringUtil.isNotEmptyOrBlank;
+
+import android.net.Uri;
 import android.widget.ImageView;
 
 import androidx.core.content.res.ResourcesCompat;
@@ -10,15 +13,20 @@ import com.squareup.picasso.Picasso;
 import org.qp.android.R;
 
 public class Bind {
+
     @BindingAdapter({"imageUrl"})
     public static void loadImage(ImageView view, String imageUrl) {
-        if (imageUrl != null && !imageUrl.isEmpty()) {
-            if (view.getId() == R.id.imageBox) {
-                Picasso.get()
+        if (isNotEmptyOrBlank(imageUrl)) {
+            switch (view.getId()) {
+                case R.id.imageBox -> Picasso.get()
                         .load(imageUrl)
                         .into(view);
-            } else {
-                Picasso.get()
+                case R.id.game_icon -> Picasso.get()
+                        .load(Uri.parse(imageUrl))
+                        .fit()
+                        .error(R.drawable.baseline_broken_image_24)
+                        .into(view);
+                default -> Picasso.get()
                         .load(imageUrl)
                         .fit()
                         .into(view);
@@ -31,4 +39,18 @@ public class Bind {
             view.setImageDrawable(drawable);
         }
     }
+
+    @BindingAdapter({"imageUri"})
+    public static void loadImage(ImageView view, Uri imageUri) {
+        if (isNotEmptyOrBlank(String.valueOf(imageUri))) {
+            Picasso.get().load(imageUri).fit().into(view);
+        } else {
+            var drawable = ResourcesCompat.getDrawable(
+                    view.getContext().getResources() ,
+                    R.drawable.baseline_broken_image_24 , null
+            );
+            view.setImageDrawable(drawable);
+        }
+    }
+
 }
