3b5f82af8a2ce0aebe7857530c05a604a8c008ab
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2025-01-26 00:44:36 +0300

    W.I.P `Downloaded games folder` | Stage zero
    * Added `MaterialBanner` library

---
+++ materialbanner-library/src/main/java/com/sergivonavi/materialbanner/BannerInterface.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Sergey Ivanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sergivonavi.materialbanner
+
+interface BannerInterface {
+
+    /**
+     * Interface used to allow the creator of a banner to run some code when a button in the
+     * banner is clicked.
+     */
+    fun interface OnClickListener {
+        /**
+         * This method will be invoked when a button in the banner is clicked.
+         *
+         * @param banner The banner that was clicked
+         */
+        fun onClick(banner: Banner)
+    }
+
+    /**
+     * Interface used to allow the creator of a banner to run some code when the banner is
+     * dismissed.
+     */
+    fun interface OnDismissListener {
+        /**
+         * This method will be invoked when the banner is dismissed.
+         *
+         * @param banner The banner that was dismissed
+         */
+        fun onDismiss(banner: Banner?)
+    }
+
+    /**
+     * Interface used to allow the creator of a banner to run some code when the banner is shown.
+     */
+    fun interface OnShowListener {
+        /**
+         * This method will be invoked when the banner is shown.
+         *
+         * @param banner The banner that is shown
+         */
+        fun onShow(banner: Banner?)
+    }
+}
---
+++ materialbanner-library/src/main/java/com/sergivonavi/materialbanner/widget/MessageView.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Sergey Ivanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sergivonavi.materialbanner.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.RestrictTo
+import androidx.appcompat.widget.AppCompatTextView
+
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+class MessageView : AppCompatTextView {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
+    )
+
+    /**
+     * Return the offset of the widget's last text line baseline from the widget's top
+     * boundary. If this widget does not support baseline alignment, this method returns -1.
+     *
+     * @return the offset of the baseline of the last text line within the widget's bounds or -1
+     * if baseline alignment is not supported
+     */
+    override fun getBaseline(): Int {
+        val layout = layout ?: return super.getBaseline()
+        val baselineOffset = super.getBaseline() - layout.getLineBaseline(0)
+        return baselineOffset + layout.getLineBaseline(layout.lineCount - 1)
+    }
+}
