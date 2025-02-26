694b8fadf344e0e9115a5468fe80d97e4d7b2b1f
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2025-02-27 01:04:09 +0300

    Remove the extra spaces

--- app/src/main/java/org/qp/android/helpers/utils/JsonUtil.java
+++ app/src/main/java/org/qp/android/helpers/utils/JsonUtil.java
@@ -11,38 +11,38 @@ import java.io.OutputStream;
 
 public class JsonUtil {
 
-    public static void objectToJson(OutputStream out , Object o) throws IOException {
+    public static void objectToJson(OutputStream out, Object o) throws IOException {
         new ObjectMapper()
                 .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .enable(SerializationFeature.INDENT_OUTPUT)
-                .writeValue(out , o);
+                .writeValue(out, o);
     }
 
-    public static void objectToJson(File file , Object o) throws IOException {
+    public static void objectToJson(File file, Object o) throws IOException {
         new ObjectMapper()
                 .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .enable(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION)
-                .writeValue(file , o);
+                .writeValue(file, o);
     }
 
     public static <T> T jsonToObject(String json, Class<T> clazz) throws IOException {
         return new ObjectMapper()
                 .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .enable(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION)
-                .readValue(json , clazz);
+                .readValue(json, clazz);
     }
 
     public static <T> T jsonToObject(File file, Class<T> clazz) throws IOException {
         return new ObjectMapper()
                 .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .enable(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION)
-                .readValue(file , clazz);
+                .readValue(file, clazz);
     }
 
     public static <T> T jsonToObject(File file, TypeReference<T> ref) throws IOException {
         return new ObjectMapper()
                 .enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
                 .enable(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION)
-                .readValue(file , ref);
+                .readValue(file, ref);
     }
 }
