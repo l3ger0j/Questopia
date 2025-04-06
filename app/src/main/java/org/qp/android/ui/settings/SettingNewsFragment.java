29510eb9a85d9a5f7f64e3c833830c02e90fc000
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2025-04-07 01:35:08 +0300

    Fix the display of the changelog fragment

--- app/src/main/java/org/qp/android/ui/settings/SettingNewsFragment.java
+++ app/src/main/java/org/qp/android/ui/settings/SettingNewsFragment.java
@@ -3,6 +3,7 @@ package org.qp.android.ui.settings;
 import static org.qp.android.helpers.utils.ColorUtil.getHexColor;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,8 @@ import androidx.navigation.Navigation;
 
 import org.qp.android.R;
 import org.qp.android.databinding.FragmentNewsBinding;
+
+import java.nio.charset.StandardCharsets;
 
 import info.hannes.changelog.ChangeLog;
 
@@ -42,18 +45,11 @@ public class SettingNewsFragment extends Fragment {
                 .addCallback(getViewLifecycleOwner(), callback);
 
         var newsBinding = FragmentNewsBinding.inflate(getLayoutInflater());
-        var css = String.format("""
-                h1 { margin-left: 0px; font-size: 1.2em; }
-                li { margin-left: 0px; }
-                ul { padding-left: 2em; }
-                body { color: %s; background-color: %s; }
-                """, getHexColor(text.data), getHexColor(background));
-        var changeLog = new ChangeLog(requireContext(), css);
-        newsBinding.newsView.loadData(
-                changeLog.getFullLog(),
-                "text/html",
-                null
-        );
+        var css = String.format("body { color: %s; background-color: %s; }", getHexColor(text.data), getHexColor(background));
+
+        var rawHtml = new ChangeLog(requireContext(), ChangeLog.DEFAULT_CSS + "\n" + css);
+        var encodedHtml = Base64.encodeToString(rawHtml.getFullLog().getBytes(StandardCharsets.UTF_8), Base64.NO_PADDING);
+        newsBinding.newsView.loadData(encodedHtml, "text/html", "base64");
 
         return newsBinding.getRoot();
     }
